fb31e7bd6375ad23f70ad8a017ce6e472bb3738e
Hardik Gupta <hardikgupta0002@gmail.com>
2025-12-14 08:32:31 +0530

    update

--- christmas-clicker-game/src/app/page.tsx
+++ christmas-clicker-game/src/app/page.tsx
@@ -4,18 +4,55 @@ import {useEffect, useState} from "react";
 
 export default function Home() {
   const [count, setCount] = useState(0); // useState!
+  const [img, setImg] = useState("1.png");
 
   useEffect(() => {
     console.log(`[${count}] boop`)
+    const audio = new Audio("/nom-nom-nom_gPJiWn4.mp3");
+    if (count > 0 ) {
+      audio.play();
+      setTimeout(() => {
+        setImg("2.png");
+      }, 100);
+      setTimeout(() => {
+        setImg("3.png");
+      }, 200);
+      setTimeout(() => {
+        setImg("4.png");
+      }, 300);
+      setTimeout(() => {
+        setImg("5.png");
+      }, 400);
+      setTimeout(() => {
+        setImg("6.png");
+      }, 500);
+      setTimeout(() => {
+        setImg("7.png");
+      }, 600);
+      setTimeout(() => {
+        setImg("8.png");
+      }, 700);
+      setTimeout(() => {
+        setImg("9.png");
+      }, 800);
+      setTimeout(() => {
+        setImg("10.png");
+      }, 900);
+      setTimeout(() => {
+        setImg("1.png");
+      }, 1000);
+    }
+
   }, [count]);
   
   return (
     <div className="min-h-screen bg-linear-to-br from-[#0f472a] to-[#1a5a3a] flex flex-col">
-      <main className="flex min-h-screen flex-col items-center justify-between py-32 px-16 bg-linear-to-br from-[#0f472a] to-[#1a5a3a]">
+      <img src={`/Image copy.png`} className="justify-start items-start w-full h-30"></img>
+      <main className="flex flex-col items-center justify-between">
         <div className="flex flex-col items-center gap-6 text-center">
-          <h1 className={"text-6xl self-center my-10"}>Haxmas Day 1</h1>
-          <p className={"text-3xl font-bold"}>Count: {count}</p> // this is how you use state variables
-          <JollyOrpheusClick onClick={() => {setCount(count + 1)}} />
+          <h1 className={"text-6xl self-center my-10 font-deca"}>Merry Christmas</h1>
+          <p className={"text-3xl font-bold"}>Count: {count}</p>
+          <JollyOrpheusClick onClick={() => {setCount(count + 1)}} img={img} />
         </div>
       </main>
     </div>
--- christmas-clicker-game/src/components/JollyOrpheusClick.tsx
+++ christmas-clicker-game/src/components/JollyOrpheusClick.tsx
@@ -2,13 +2,14 @@
 import Image from "next/image";
 
 type JollyOrpheusClickProps = {
-	onClick: () => void; // setting the type of onClick to a function
+	onClick: () => void;
+	img: string;
 };
 
-export default function JollyOrpheusClick({ onClick }: JollyOrpheusClickProps) {
+export default function JollyOrpheusClick({ onClick, img }: JollyOrpheusClickProps) {
 	return (
 		<button onClick={onClick}>
-			<Image src="/jollyorph.png" width={512} height={512} alt="Jolly Orpheus"/> //jollyorph.png is located in public/!!!!
+			<Image src={`/cookie/${img}`} width={250} height={250} alt="Jolly Orpheus"/>
 		</button>
 	)
 }
